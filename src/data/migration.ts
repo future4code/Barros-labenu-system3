fbb2984d42330813d1768b91b1547a5643efd88f
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-15 10:40:32 -0300

    tipei a models... e deixei o migrations como anteriormente

--- src/data/migration.ts
+++ src/data/migration.ts
@@ -1,114 +1,111 @@
 import connection from "./connetion"
-import { docente, docenteEspecialidade, especialidade, estudante, estudanteHobby, hobby, Turma } from "./dados"
-// import estudante from "./estudante.json"
-// import turma from "./turma.json"
-// import hobby from "./hobby.json"
-// import hobbyEstudante from "./hobbyEstudante.json"
-// import docente from "./docente.json"
-// import especialidade from "./especialidade.json"
-// import docenteEspecialidade from "./docenteEspecialidade.json"
-import { TABLE_CLASS, TABLE_HOBBY, TABLE_HOBBY_STUDENT, TABLE_INSTRUCTOR, TABLE_INSTRUCTOR_SPEC, TABLE_SPECIALTY, TABLE_STUDENT } from "./tableNames"
+import estudante from "./estudante.json"
+import turma from "./turma.json"
+import hobby from "./hobby.json"
+import hobbyEstudante from "./hobbyEstudante.json"
+import docente from "./docente.json"
+import especialidade from "./especialidade.json"
+import docenteEspecialidade from "./docenteEspecialidade.json"
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
 
-const createTables = async () => {await connection.raw(`
+const createTables = () => connection.raw(`
 
-  DROP TABLE IF EXISTS ${TABLE_STUDENT},${TABLE_CLASS},${TABLE_INSTRUCTOR_SPEC},${TABLE_INSTRUCTOR},${TABLE_SPECIALTY},${TABLE_HOBBY},${TABLE_HOBBY_STUDENT}
-
-  CREATE TABLE ${TABLE_CLASS}(
+  CREATE TABLE turma(
     id VARCHAR(255) PRIMARY KEY,
-    nome VARCHAR(255)  NOT NULL UNIQUE,
-    modulo VARCHAR(255) DEFAULT 0
+    nome VARCHAR(255) NOT NULL,
+    modulo VARCHAR(255) NOT NULL DEFAULT 0
 );
 
-  CREATE TABLE ${TABLE_STUDENT}(
+  CREATE TABLE estudante(
     id VARCHAR(255) PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    data_nasc VARCHAR(255) NOT NULL,
+    data_nasc DATE NOT NULL,
     turma_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY(turma_id) REFERENCES ${TABLE_CLASS}(id)
+    FOREIGN KEY(turma_id) REFERENCES turma(id)
  );
  
-  CREATE TABLE ${TABLE_HOBBY}(
-    id VARCHAR(255) PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
-
-  CREATE TABLE ${TABLE_HOBBY_STUDENT}(
-    id VARCHAR(255) PRIMARY KEY,
-    id_estudante VARCHAR(255) NOT NULL,
-    id_hobby VARCHAR(255) NOT NULL,
-    FOREIGN KEY(id_estudante) REFERENCES ${TABLE_STUDENT}(id),
-    FOREIGN KEY(id_hobby) REFERENCES ${TABLE_HOBBY}(id)
-);
-
- CREATE TABLE ${TABLE_INSTRUCTOR}(
-    id VARCHAR(255) PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    data_nasc VARCHAR(255) NOT NULL,
-    turma_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY(turma_id) REFERENCES ${TABLE_CLASS}(id)
-);
-
-
-  CREATE TABLE ${TABLE_SPECIALTY}(
+  CREATE TABLE hobby(
     id VARCHAR(255) PRIMARY KEY,
     nome VARCHAR(255) NOT NULL UNIQUE
 );
 
-  CREATE TABLE ${TABLE_INSTRUCTOR_SPEC }(
+  CREATE TABLE hobby_estudante(
+    id VARCHAR(255) PRIMARY KEY,
+    id_estudante VARCHAR(255) NOT NULL,
+    id_hobby VARCHAR(255) NOT NULL,
+    FOREIGN KEY(id_estudante) REFERENCES estudante(id),
+    FOREIGN KEY(id_hobby) REFERENCES hobby(id)
+);
+
+ CREATE TABLE docente(
+    id VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    data_nasc DATE NOT NULL,
+    id_turma VARCHAR(255) NOT NULL,
+    FOREIGN KEY(id_turma) REFERENCES turma(id)
+);
+
+
+
+  CREATE TABLE especialidade(
+    id VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+  CREATE TABLE docente_especialidade(
     id VARCHAR(255) PRIMARY KEY,
     id_docente VARCHAR(255) NOT NULL,
     id_especialidade VARCHAR(255) NOT NULL,
-    FOREIGN KEY (id_docente) REFERENCES ${TABLE_INSTRUCTOR}(id),
-    FOREIGN KEY (id_especialidade) REFERENCES ${TABLE_SPECIALTY}(id)
+    FOREIGN KEY (id_docente) REFERENCES docente(id),
+    FOREIGN KEY (id_especialidade) REFERENCES especialidade(id)
 )
 
  `)
- .then(() => { console.log("Tabela criada com sucesso!") })
- .catch(printError)
-}
 
-const insertTurma = async () => await connection(TABLE_CLASS)
-  .insert(Turma)
+  .then(() => { console.log("Tabela criada com sucesso!") })
+  .catch(printError)
+
+const insertTurma = () => connection("turma")
+  .insert(turma)
   .then(() => { console.log("Turmas criadas com sucesso!") })
   .catch(printError)
 
-const insertEstudante = async () => await connection(TABLE_STUDENT)
+const insertEstudante = () => connection("estudante")
   .insert(estudante)
   .then(() => { console.log("Estudante criados com sucesso!") })
   .catch(printError)
 
-const insertHobby = async () => await connection(TABLE_HOBBY)
+const insertHobby = () => connection("hobby")
   .insert(hobby)
   .then(() => { console.log("Hobby criados com sucesso!") })
   .catch(printError)
 
-const insertHobbyEstudante = async () => await connection(TABLE_HOBBY_STUDENT )
-  .insert(estudanteHobby)
+const insertHobbyEstudante = () => connection("hobby_estudante")
+  .insert(hobbyEstudante)
   .then(() => { console.log("Hobby inserido com sucesso!") })
   .catch(printError)
 
-const insertDocente = async () => await connection(TABLE_INSTRUCTOR)
+const insertDocente = () => connection("docente")
   .insert(docente)
   .then(() => { console.log("Docente inserido com sucesso!") })
   .catch(printError)
 
-const insertEspecialidade = async () => await connection(TABLE_SPECIALTY)
+const insertEspecialidade = () => connection("especialidade")
   .insert(especialidade)
   .then(() => { console.log("Especialidade criada com sucesso!") })
   .catch(printError)
 
-const insertEspecialidadeDocente = async () => await connection(TABLE_INSTRUCTOR_SPEC)
+const insertEspecialidadeDocente = () => connection("docente_especialidade")
   .insert(docenteEspecialidade)
   .then(() => { console.log("Especialidade inserida com sucesso!") })
   .catch(printError)
 
-const fimConeccao = async () => {
-  console.log("Fim do banco de dados!");
-  await connection.destroy()
+const fimConeccao = () => {
+  console.log("Inserido ao banco com sucesso!");
+  connection.destroy()
 
 }
 
